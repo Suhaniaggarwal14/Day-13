94045967cfc0955dc53fe5b1905068eca9d35d1e
Suhaniaggarwal14 <140490968+Suhaniaggarwal14@users.noreply.github.com>
2024-07-31 19:56:11 +0200

    Add files via upload

---
+++ Day13/demoOnBuiltFunction.js
@@ -0,0 +1,38 @@
+//number related fuctions
+console.log(Math.pow(3,4))
+console.log(Math.sqrt(441))
+console.log(Math.floor(35.65678))
+console.log(Math.ceil(35.65678))
+console.log(Math.round(35.65678))
+console.log(Math.floor(Math.random()*10+1))
+
+//string related functions
+let str="welcome"
+console.log(str.length)
+console.log(str.toUpperCase())
+console.log(str.toLowerCase())
+str="hello world"
+console.log(str.replace('world','suhani'))
+console.log(str.split("").length)
+console.log(str.search("world"))
+console.log(tr.concat(" of Java Script"))
+console.log(str.slice(3,6))
+
+//date functions
+console.log(new Date())
+let mydate=new Date();
+let strDate='2020-06-12'
+console.log(new Date(strDate))
+
+//Array Functions
+let arr=[21,12,30,15,6]
+console.log(arr)
+console.log(arr.indexOf(16))
+console.log(arr.slice(2,4))
+console.log(arr.splice(2,4))
+console.log(arr)
+
+let emp={name:'suhani', id:123,salary:50000}
+console.log(emp.name)
+console.log(emp.salary)
+console.log(emp.name)
---
+++ Day13/demoOnlet.js
@@ -0,0 +1,8 @@
+{
+    let i=5;   //let is a block scope ,var is at function scope
+console.log(i)
+const pi=3.14//cant change value and scoe is within block
+console.log(pi)
+}
+console.log(pi)
+//console.log(i)
